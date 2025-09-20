b0b16443a95de998d0564892b37400e261c3be52
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-09-20 20:11:35 -0300

    Adding plots relate to credit card limti with other columns. Add nulls values on the plot to be analyzed on age.

--- src/ifood_case/data_visualizer.py
+++ src/ifood_case/data_visualizer.py
@@ -1,11 +1,24 @@
+"""Módulo responsável por criações das visualizações para insigts sobre os dados."""
 import matplotlib.pyplot as plt
 import seaborn as sns
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col, concat_ws, explode
+from pyspark.sql.functions import col, concat_ws, explode, when
 
 IFOOD_RED = "#EA1D2C"
 IFOOD_BLACK = "#3F3E3E"
+IFOOD_GRAY_LIGHT = "#E0E0E0"
+IFOOD_ORANGE = "#FF7C00"
+IFOOD_LIGHT_RED = "#F9868D"
 
+
+IFOOD_PALETTE = [
+    IFOOD_RED,
+    "#FF7043",
+    "#FFA726",
+    "#D3D3D3",
+    "#8B0000",
+    "#696969",
+]
 
 class DataVisualizer:
     """Uma classe para criar visualizações a partir de dados de ofertas,
@@ -55,10 +68,13 @@ class DataVisualizer:
 
     def __plot_barplot_channels_joined(self) -> None:
         """Plota a contagem de combinações de canais (ex: 'email, web')."""
-        channels_pd = self.offers \
-            .withColumn("channels_str", concat_ws(", ", col("channels"))) \
-            .groupBy("channels_str").count().orderBy("count", ascending=False) \
+        channels_pd = (
+            self.offers.withColumn("channels_str", concat_ws(", ", col("channels")))
+            .groupBy("channels_str")
+            .count()
+            .orderBy("count", ascending=False)
             .toPandas()
+        )
 
         plt.figure(figsize=(10, 6))
         sns.barplot(data=channels_pd, x="channels_str", y="count", color=IFOOD_RED)
@@ -84,8 +100,7 @@ class DataVisualizer:
         plt.tight_layout()
         plt.show()
 
-
-    def plot_histogram_age(self):
+    def plot_histogram_age(self) -> None:
         """Plota a distribuição de idade por perfil."""
         age = self.profile.select(col("age")).toPandas()
 
@@ -97,7 +112,7 @@ class DataVisualizer:
         plt.tight_layout()
         plt.show()
 
-    def plot_histogram_credit_card_limit(self):
+    def plot_histogram_credit_card_limit(self) -> None:
         """Plota a distribuição de limite de cartão de crédito por perfil."""
         limit = self.profile.select(col("credit_card_limit")).toPandas()
 
@@ -109,12 +124,9 @@ class DataVisualizer:
         plt.tight_layout()
         plt.show()
 
-
-    def plot_barplot_gender(self):
+    def plot_barplot_gender(self) -> None:
         """Plota a contagem de gênero dentro dos perfis."""
-        gender = self.profile.select(col("gender")) \
-                             .groupBy("gender").count() \
-                             .na.fill({"gender": "Nulo"}).toPandas()
+        gender = self.profile.select(col("gender")).groupBy("gender").count().na.fill({"gender": "Nulo"}).toPandas()
 
         plt.figure(figsize=(10, 6))
         sns.barplot(data=gender, x="gender", y="count", color=IFOOD_RED)
@@ -125,12 +137,9 @@ class DataVisualizer:
         plt.tight_layout()
         plt.show()
 
-
-    def plot_barplot_event(self):
+    def plot_barplot_event(self) -> None:
         """Plota a contagem de tipos de events dentro de transações."""
-        event = self.transactions.select(col("event")) \
-                             .groupBy("event").count() \
-                             .na.fill({"event": "Nulo"}).toPandas()
+        event = self.transactions.select(col("event")).groupBy("event").count().na.fill({"event": "Nulo"}).toPandas()
 
         plt.figure(figsize=(10, 6))
         sns.barplot(data=event, x="event", y="count", color=IFOOD_RED)
@@ -138,6 +147,42 @@ class DataVisualizer:
         plt.xlabel("Eventos", color=IFOOD_BLACK)
         plt.ylabel("Contagem", color=IFOOD_BLACK)
         plt.xticks(rotation=45, ha="right")
+        plt.tight_layout()
+        plt.show()
+
+    def plot_histogram_age_credit_card_limit(self) -> None:
+        limit_age = (
+            self.profile.select(col("age"), col("credit_card_limit"))
+            .withColumn(
+                "age_group",
+                when(col("age") < 18, "Menor de 18")
+                .when((col("age") >= 18) & (col("age") <= 25), "18-25")
+                .when((col("age") >= 26) & (col("age") <= 35), "26-35")
+                .when((col("age") >= 36) & (col("age") <= 50), "36-50")
+                .when(col("age") > 50, "51+")
+                .otherwise("Não Informado"),
+            )
+            .toPandas()
+        )
+
+
+        plt.figure(figsize=(10, 6))
+        sns.histplot(data=limit_age, x="credit_card_limit", hue="age_group", palette=IFOOD_PALETTE)
+        plt.title("Distribuição de limite de cartão de crédito por idade", color=IFOOD_BLACK)
+        plt.xlabel("Limite", color=IFOOD_BLACK)
+        plt.ylabel("Contagem", color=IFOOD_BLACK)
+        plt.tight_layout()
+        plt.show()
+
+    def plot_histogram_gender_credit_card_limit(self) -> None:
+        limit_gender = self.profile.select(col("gender"), col("credit_card_limit")) \
+                                   .na.fill({"gender": "Nulo"}).toPandas()
+
+        plt.figure(figsize=(10, 6))
+        sns.histplot(data=limit_gender, x="credit_card_limit", hue="gender", palette=IFOOD_PALETTE)
+        plt.title("Distribuição de limite de cartão de crédito por gênero", color=IFOOD_BLACK)
+        plt.xlabel("Limite", color=IFOOD_BLACK)
+        plt.ylabel("Contagem", color=IFOOD_BLACK)
         plt.tight_layout()
         plt.show()
 
