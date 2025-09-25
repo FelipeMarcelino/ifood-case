f8aa396bfc3360fd1177f04af93e6ce8784d5cea
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-09-25 11:20:22 -0300

    calibrated prob implemented

--- src/ifood_case/model_trainer.py
+++ src/ifood_case/model_trainer.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Optional
 
 import lightgbm as lgb
+import numpy as np
 import pandas as pd
 from optuna.distributions import FloatDistribution, IntDistribution
 from optuna.integration import OptunaSearchCV
@@ -9,6 +10,8 @@ from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
 from sklearn.compose import ColumnTransformer
+from sklearn.isotonic import IsotonicRegression
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 
@@ -21,34 +24,53 @@ class ModelTrainer(ABC):
         self._target = target
         self._best_params: Optional[dict[str, float]] = None
         self._best_score: Optional[float] = None
+        self._estimator: Optional[Pipeline] = None
+        self._calibrator: Optional[IsotonicRegression] = None
 
     @property
     def best_params(self):
-        if self.best_params is None:
-            print("Model not tuned yet.")
+        if self._best_params is None:
+            raise RuntimeError("Model not tuned yet. Call train() first")
         return self._best_params
 
     @property
     def best_score(self):
-        if self.best_score is None:
-            print("Model not tuned yet.")
+        if self._best_score is None:
+            raise RuntimeError("Model not tuned yet. Call train() first")
         return self._best_score
 
-    def predict(self):
-        self.model.predict()
+    def predict(self, df: pd.DataFrame, threshold: float = 0.5):
+        calibrated_probs_pos = self.predict_proba(df)[:, 1]
+        return (calibrated_probs_pos >= threshold).astype(int)
 
-    def predict(self):
-        self.model.predict_proba()
 
-    def train(self):
-        x_train, x_test, y_train, y_test = self.__split_data()
+    def predict_proba(self, df: pd.DataFrame):
+        if self._estimator is None or self._calibrator is None:
+            raise RuntimeError("Model or calibrator has not been trained. Call train() first.")
+
+        uncalibrated_probs = self._estimator.predict_proba(df)[:, 1]
+
+        calibrated_probs_pos = self._calibrator.predict(uncalibrated_probs)
+
+        return np.vstack([1 - calibrated_probs_pos, calibrated_probs_pos]).T
+
+    def train(self) -> tuple[pd.DataFrame, pd.DataFrame]:
+        x_train, x_test, x_calib, y_train, y_test, y_calib = self._split_data()
         pipeline = self._create_pipeline()
         optuna_search = self._tune_model(pipeline, x_train, y_train)
 
         self._best_params = optuna_search.best_params_
         self._best_score = optuna_search.best_score_
+        self._estimator = optuna_search.best_estimator_
 
-    def _split_data(self) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+        uncalibrated_probs = self._estimator.predict_proba(x_calib)[:, 1]
+
+        self._calibrator = IsotonicRegression(y_min=0, y_max=1, out_of_bounds="clip")
+        self._calibrator.fit(uncalibrated_probs, y_calib)
+
+        return x_test, y_test
+
+    def _split_data(self) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.Series, pd.Series, pd.Series]:
         window_spec = Window.partitionBy("account_id")
 
         df_with_max_time = self._df.withColumn(
@@ -64,13 +86,20 @@ class ModelTrainer(ABC):
             "max_time_received",
         )
 
-        x_train = train_df.loc[self._numerical_columns + self._categorical_columns]
-        x_test = test_df.loc[self._numerical_columns + self._categorical_columns]
+        train_df = train_df.toPandas()
+        test_df = test_df.toPandas()
 
-        y_train = train_df.loc[self._target]
-        y_test = test_df.loc[self._target]
+        x_train = train_df[self._numerical_columns + self._categorical_columns]
+        x_test = test_df[self._numerical_columns + self._categorical_columns]
 
-        return x_train, x_test, y_train, y_test
+        y_train = train_df[self._target]
+        y_test = test_df[self._target]
+
+        x_train, x_calib, y_train, y_calib = train_test_split(
+            x_train, y_train, test_size=0.2, random_state=42, stratify=y_train,
+        )
+
+        return x_train, x_test, x_calib, y_train, y_test, y_calib
 
     def _create_pipeline(self) -> Pipeline:
         preprocessor = ColumnTransformer(
@@ -95,12 +124,12 @@ class LGBMTrainer(ModelTrainer):
     def __init__(self, df: DataFrame, numerical_columns: list[str], categorical_columns: list[str], target):
         super().__init__(df, numerical_columns, categorical_columns, target)
 
-        self.model = lgb.LGBMClassifier(random_state=42)
+        self._model = lgb.LGBMClassifier(random_state=42)
 
     def _create_pipeline(self) -> Pipeline:
         pipeline = super()._create_pipeline()
 
-        pipeline.steps.append(("classifier"), self.model)
+        pipeline.steps.append(("classifier", self._model))
 
         return pipeline
 
@@ -117,11 +146,11 @@ class LGBMTrainer(ModelTrainer):
         optuna_search = OptunaSearchCV(
             estimator=pipeline,
             param_distributions=param_distributions,
-            n_iter=50,
+            n_trials=50,
             cv=3,
             scoring="average_precision",
             random_state=42,
-            verbose=1,
+            verbose=0,
         )
 
         optuna_search.fit(x_train, y_train)
